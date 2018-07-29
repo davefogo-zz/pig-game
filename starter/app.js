b84dd1d203ecdd43fb7b2ff338b8d08042511682
David Forero <davefogo@gmail.com>
2018-07-28 21:17:20 -0500

    Fix state variable bug

--- starter/app.js
+++ starter/app.js
@@ -9,8 +9,9 @@ GAME RULES:
 
 */
 
-var scores, roundScore, activePlayer;
+var scores, roundScore, activePlayer, gamePlaying;
 var init = function() {
+  gamePlaying = true;
   scores = [0,0];
   roundScore = 0;
   activePlayer = 0;
@@ -19,10 +20,13 @@ var init = function() {
   document.getElementById('score-1').textContent = 0;
   document.getElementById('current-0').textContent = 0;
   document.getElementById('current-1').textContent = 0;
-  document.querySelector('.player-0-panel').classList.add('active');
-  document.querySelector('.player-1-panel').classList.remove('active');
+  document.getElementById('name-0').textContent = 'Player 1';
+  document.getElementById('name-1').textContent = 'Player 2';
   document.querySelector('.player-0-panel').classList.remove('winner');
   document.querySelector('.player-1-panel').classList.remove('winner');
+  document.querySelector('.player-0-panel').classList.remove('active');
+  document.querySelector('.player-1-panel').classList.remove('active');
+  document.querySelector('.player-0-panel').classList.add('active');
   document.querySelector('.dice').style.display = 'none';
 }
 var nextPlayer = function() {
@@ -35,50 +39,52 @@ var nextPlayer = function() {
   roundScore = 0;
 }
 
-
 init();
 
 //hide the dice
 document.querySelector('.dice').style.display = 'none';
-
 document.getElementById('score-0').textContent = '0';
 document.getElementById('score-1').textContent = '0';
 document.getElementById('current-0').textContent = '0';
 document.getElementById('current-1').textContent = '0';
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
-  //1. get random number
-  var dice = Math.floor(Math.random() * 6) + 1;
-  //2. display the random number
-  var diceDOM = document.querySelector('.dice');
-  diceDOM.src = 'dice-' + dice + '.png';
-  diceDOM.style.display = 'block';
-  //3. update the round score if it's not 1
-  if (dice === 1) {
-    nextPlayer();
-  } else {
-    //Add score to 0
-    roundScore += dice;
-    document.querySelector('#current-' + activePlayer).textContent = roundScore;
+  if (gamePlaying) {
+    //1. get random number
+    var dice = Math.floor(Math.random() * 6) + 1;
+    //2. display the random number
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.src = 'dice-' + dice + '.png';
+    diceDOM.style.display = 'block';
+    //3. update the round score if it's not 1
+    if (dice === 1) {
+      nextPlayer();
+    } else {
+      //Add score to 0
+      roundScore += dice;
+      document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    }
   }
 })
 
 document.querySelector('.btn-hold').addEventListener('click', function() {
-  //Add the current score to players GLOBAL score
-  scores[activePlayer] += roundScore;
-  document.getElementById('score-' + activePlayer).textContent = scores[activePlayer];
-  //Check if player won the game
-  if (scores[activePlayer] >= 20) {
-    document.getElementById('name-' + activePlayer).textContent = 'WINNER!';
-    document.querySelector('.dice').style.display = 'none';
-    document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
-    document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
-  } else {
-    //Update the UI
+  if (gamePlaying) {
+    //Add the current score to players GLOBAL score
+    scores[activePlayer] += roundScore;
     document.getElementById('score-' + activePlayer).textContent = scores[activePlayer];
-
-    //Next player
-    nextPlayer();
+    //Check if player won the game
+    if (scores[activePlayer] >= 100) {
+      document.getElementById('name-' + activePlayer).textContent = 'WINNER!';
+      document.querySelector('.dice').style.display = 'none';
+      document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+      document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+      gamePlaying = false;
+    } else {
+      //Update the UI
+      document.getElementById('score-' + activePlayer).textContent = scores[activePlayer];
+      //Next player
+      nextPlayer();
+    }
   }
 })
 
